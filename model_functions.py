42f915db5e77e2482ecc63eb9e44e4517cd5c569
david <a21820888@gmail.com>
2023-09-20 13:28:08 -0700

    update

--- model_functions.py
+++ model_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Bouyancy functions
 def approxfqv(z, c):
     # Dr.Gerardo Hernandez Dueñas
     a = 18.04
@@ -13,14 +14,38 @@ def approxfqv(z, c):
         return fqv
     else:
         return "ERROR"
+
+def theta_hat(z,par):
+    theta_0 = par[0]
+    tem_rate = par[1]
+    return theta_0 + tem_rate * z
+
+
+def bouyancy_force(par, qv, qr, z):
     
+
+
+
+
+
+
+# aerosol and terminal velocity functions 
 def vt(qr,par):
     vt0 = par[0]
     q_star = par[1]
     y = vt0 * qr / q_star
     return y
     
+def vtn(qr,par):
+    q_star = par[1]
+    vtnd = par[2]
+    y = vtnd + np.max([qr / q_star, 1]) * np.max([vt(qr,par)-vtnd],0)
 
+def w_vt(w,qr,par):
+    y = w - vt(qr, par)
+    return y
 
-def vtn(qr,vtnd):
-    
+def w_vtn(w, qr, par):
+    y = w - vtn(qr, par)
+    return y
+
--- parameters.py
+++ parameters.py
@@ -2,44 +2,48 @@ import scale
 import numpy as np
 
 
-# B
-temperature_rate = 3  # K / km
-temperature_rate = temperature_rate * scale.height / scale.temperature
+# Bouyancy and friction parameters
+temperature_ref = 300  # K
+temperature_rate = 3   # K / km
+latent_heat = 2.5e6    # Calor latente
+specific_heat = 1e3    # Calor específico a presión constante.
+epsilon = 0.6
+g = 9.81               # m/s^2
 
-# L, C_p
-latent_heat = 2.5e6  # Calor latente
+g = (g / scale.velocity) * (60 * scale.time)
+temperature_ref = temperature_ref / scale.temperature
+temperature_rate = temperature_rate * scale.height / scale.temperature
 latent_heat = scale.mass / scale.energy * latent_heat
-specific_heat = 1e3  # Calor específico a presión constante.
 specific_heat = (specific_heat / scale.energy) * (scale.mass * scale.temperature)
 
 lcp = latent_heat / specific_heat
 
-# epsilon
-epsilon = 0.6
+desaceleration = 7.5  # min
+desaceleration = desaceleration / scale.time
 
-# tau_w
-tau_w = 7.5  # min
-tau_w = tau_w / scale.time
-
-# gravity
-g = 9.81  # m/s^2
-g = (g / scale.velocity) * 60 * scale.time
+bvp = np.array([temperature_ref, temperature_rate, g, epsilon, lcp, desaceleration])
 
 
-q_star = 10
-q_star = q_star / scale.ratio
-
-
-b_w = 0.491  # m/s sqrt(s)
-b_w = (b_w / scale.velocity) * scale.time * 60 * np.sqrt(60 * scale.time)
-
-tau0 = 0.1
+# condensation and evaporation parameters
+tau_0 = 0.1
 gamma = 0.3
-taue = 0.2
+tau_e = 0.2
 c = 0.2
-c0 = 0.4
+c_0 = 0.4
 
+cep = np.array([tau_0, gamma, tau_e, c, c_0])
 
+# aerosol and terminal velocity parameters
 vt0 = 1
+q_star = 10
 vtnd = 1
 
+vt0 = vt0 / scale.velocity
+q_star = q_star / scale.ratio
+vtnd = vtnd / scale.velocity
+
+atvp = np.array([vt0, q_star, vtnd])
+
+# stochastic parameters
+b_w = 0.491  # m/s sqrt(s)
+b_w = (b_w / scale.velocity) * scale.time * 60 * np.sqrt(60 * scale.time)
